4bdd7c8ef83a6bf0c892135d33fed55cff6c8a21
jimmyjames88 <me@james-allen.ca>
2023-12-18 11:59:37 -0700

    Invoice index tweaks to work with DataTable + search. Tweaked invoice seeder refNo's

--- src/controllers/InvoiceController.ts
+++ src/controllers/InvoiceController.ts
@@ -1,18 +1,45 @@
 import { Request, Response } from 'express'
+import { Op, FindOptions } from 'sequelize'
 import Invoice from '@models/Invoice'
 import Client from '@models/Client'
 import User from '@models/User'
 
 export default {
   async index(req: Request, res: Response) {
-    const invoices = await Invoice.findAll({ 
-      where: { userId: Number(req.body.userId) },
-      include: [{ model: Client }]
+    const options: FindOptions  = {
+      where: { 
+        userId: Number(req.body.userId),
+      },
+      offset: (Number(req.query.page) - 1) * Number(req.query.itemsPerPage || 10) || 0,
+      limit: Number(req.query.itemsPerPage) || 10,
+      include: [{
+        model: Client,
+        attributes: [ 'id', 'name' ],
+        as: 'client'
+      }]
+    } 
+    if (req.query.search) {
+      options.where = {
+        ...options.where,
+        [Op.or]: [
+          { 'refNo': { [Op.like]: '%' + req.query.search + '%' } },
+          { '$client.name$': { [Op.like]: '%' + req.query.search + '%' } }
+        ]
+      }
+    }
+    if (req.query.sortBy) {
+      const [ sortBy ] = req.query.sortBy as any
+      options.order = [ [ sortBy.key, sortBy.order ] ]
+    }
+
+    const invoices = await Invoice.findAll(options)
+    const total = await Invoice.count({ include: Client, where: options.where })
+    if (!invoices)
+      return res.sendStatus(404)
+    return res.json({
+      items: invoices,
+      total
     })
-    if (invoices)
-      return res.send(invoices)
-    return res.sendStatus(404)
-    
   },
 
   async show(req: Request, res: Response) {
--- src/database/seeders/20231205040037-Invoice.js
+++ src/database/seeders/20231205040037-Invoice.js
@@ -4,11 +4,12 @@ const { faker } = require('@faker-js/faker')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    const invoiceNum = Math.floor(Math.random()*90000) + 10000;
     const data = [...Array(300).keys()].map((i) => {
       const subtotal = parseFloat(faker.finance.amount())
       const status = i % 2 === 0 ? 'DRAFT' : 'SENT'
       return {
-        refNo: `INV-${i}`,
+        refNo: `INV-${invoiceNum + i}`,
         status,
         date: faker.date.past(),
         notes: faker.lorem.sentence(),
