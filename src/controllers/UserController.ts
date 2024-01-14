4a4f7647175482138a1919d8dd603005918f7076
jimmyjames88 <me@james-allen.ca>
2024-01-14 16:43:22 -0700

    Updated Type to user Model in UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from 'express'
-import { _User } from '@jimmyjames88/freebooks-types'
 import User from '@models/User'
 import Profile from '@models/Profile'
 
 export default {
   async show(req: Request, res: Response) {
-    const user: _User | null = await User.findByPk(req.body.userId, { include: Profile })
+    const user: User | null = await User.findByPk(req.body.userId, { include: Profile })
     if (user) {
       return res.json({
         id: user.id,
@@ -30,24 +29,6 @@ export default {
       return res.status(200).json(user)
     }
     return res.status(404).json({})
-
-    // const { name, profile }: _User = req.body
-    // const user = await User.update({
-    //   name
-    // }, {
-    //   where: {
-    //     id: Number(req.body.userId) 
-    //   },
-    // })
-    // if (user) {
-    //   await Profile.update({ ...profile }, {
-    //     where: {
-    //       userId: Number(req.body.userId)
-    //     }
-    //   })
-    //   return res.status(200)
-    // }
-    // return res.status(404).json({})
   },
 
   async destroy(req: Request, res: Response) {
